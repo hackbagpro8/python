b52f0c9feaed0116d11b4e0f9fc75139a9e3676a
hackbagpro8 <manitkejriwal@gmail.com>
2025-09-27 12:01:27 +0530

    Add files via upload

---
+++ class 10/to find the mid number.py
@@ -0,0 +1,11 @@
+num=int(input("enter the number : "))
+
+str1=str(num)
+len=len(str1)
+
+if len%2 ==0:
+    mid=int(len/2)-1
+else:
+    mid=int(len/2)
+midproduct=int(str1[mid])*int(str1[mid+1])
+print("the mid producxt id",midproduct)
