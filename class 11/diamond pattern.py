6c2cf78d8157874c283f7a3737855b5a97aad3f2
hackbagpro8 <manitkejriwal@gmail.com>
2025-09-28 12:03:32 +0530

    Add files via upload

---
+++ class 11/diamond pattern.py
@@ -0,0 +1,19 @@
+n=int(input("enter the no of rows : "))
+
+space=25
+for i in range(1,n+1,2):
+    for k in range(space):
+        print(end=" ")
+    for j in range(1,i+1):
+        print(j,end = "")
+    print()
+    space -=1
+
+space +=2
+for i in range(n-2,0,-2):
+    for k in range(space):
+        print(end=" ")
+    for j in range(1,i+1):
+        print(j,end = "")
+    print()
+    space +=1
