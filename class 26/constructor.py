8f0c2c79c982eb68893ca51b92d5b09ae2dca1a6
hackbagpro8 <manitkejriwal@gmail.com>
2025-11-22 13:02:44 +0530

    Add files via upload

---
+++ class 26/constructor.py
@@ -0,0 +1,15 @@
+class iostring:
+    def __init__(self):
+        self.str1 = ""
+    def getstr(self):
+        self.str1 = input("enter the string : ")
+    def print_str(self):
+        print(f"The upper case of {self.str1} is {self.str1.upper()}")
+
+
+obj1 = iostring()
+obj1.getstr()
+obj1.print_str()
+obj2 = iostring()
+obj2.getstr()
+obj2.print_str()
